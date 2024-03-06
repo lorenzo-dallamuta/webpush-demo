5cd93f9dd0b67c98f388ca34b768e475eab7bd57
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2024-03-06 14:43:12 +0100

    feat: implement crud api endpoints to save subscriptions strings to local db

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,18 +1,27 @@
 import createError, { HttpError } from 'http-errors';
 import express, { NextFunction, Request, Response } from 'express'
+import betterSqlLite from 'better-sqlite3';
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
 import logger from 'morgan'
+import webpush from "web-push"
 
 import indexRouter from './routes/index'
+import pushRouter from './routes/push'
+
+import dotenv from 'dotenv'
+dotenv.config()
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.use('/push', pushRouter);
 
 // catch 404 and forward to error handler
 app.use(function (_req: Request, _res: Response, next: NextFunction) {
@@ -35,5 +44,16 @@ app.use(function (err: HttpError, req: Request, res: Response, _next: NextFuncti
     name: err.name
   });
 });
+
+const db = new betterSqlLite("push.sqlite");
+db.pragma('journal_mode = WAL');
+db.exec(/* sql */`CREATE TABLE IF NOT EXISTS subscriptions (subscription TEXT UNIQUE)`);
+db.close();
+
+webpush.setVapidDetails(
+  process.env.VAPID_SUBJECT!,     // mail
+  process.env.VAPID_PUBLIC_KEY!,  // public
+  process.env.VAPID_PRIVATE_KEY!, // private
+);
 
 export default app;
---
+++ packages/server/src/routes/push.ts
@@ -0,0 +1,124 @@
+import express, { Request, Response } from 'express'
+import { PushSubscription } from "web-push"
+import Database from 'better-sqlite3';
+
+var router = express.Router();
+
+type SubscriptionEntry = { id: number, subscription: string }
+
+function deleteSubscriptionFromDatabase(id: number) {
+  return new Promise<void>(function (resolve, reject) {
+    try {
+      const db = new Database("push.sqlite");
+      db.pragma('journal_mode = WAL');
+      db.prepare(/* sql */`DELETE FROM subscriptions WHERE rowId = (?)`).run(id)
+      db.close()
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  })
+}
+
+function getAllSubscriptionsFromDatabase() {
+  return new Promise<SubscriptionEntry[]>(function (resolve, reject) {
+    try {
+      const db = new Database("push.sqlite");
+      db.pragma('journal_mode = WAL');
+      const entries = db
+        .prepare(/* sql */`SELECT rowId as id, subscription FROM subscriptions`)
+        .all()
+      db.close()
+      resolve(entries as SubscriptionEntry[]);
+    } catch (err) {
+      reject(err);
+    }
+  })
+}
+
+function saveSubscriptionToDatabase(subscription: PushSubscription) {
+  return new Promise<PushSubscription>(function (resolve, reject) {
+    try {
+      const db = new Database("push.sqlite");
+      db.pragma('journal_mode = WAL');
+      const { lastInsertRowid } = db
+        .prepare(/* sql */`INSERT INTO subscriptions(subscription) VALUES(?)`)
+        .run(JSON.stringify(subscription));
+      const inserted = db
+        .prepare(/* sql */`SELECT rowId as id, * FROM subscriptions WHERE rowid = (?)`)
+        .get(lastInsertRowid) as { id: number, subscription: string };
+      db.close()
+      resolve(JSON.parse(inserted.subscription))
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+/* GET subscription objects. */
+router.get('/', function (_req: Request, res: Response) {
+  getAllSubscriptionsFromDatabase()
+    .then(function (entries) {
+      res.status(201);
+      res.setHeader('Content-Type', 'application/json');
+      res.json(entries.map((e) => (JSON.parse(e.subscription))));
+    })
+    .catch(function (err) {
+      res.status(500);
+      res.setHeader('Content-Type', 'application/json');
+      res.json({
+        error: {
+          id: 'unable-to-get-subscriptions',
+          message: 'We were unable to recover the subscriptions from our database.',
+        },
+      });
+    });
+});
+
+/* POST subscription object. */
+router.post('/', function (req: Request, res: Response) {
+  saveSubscriptionToDatabase(req.body)
+    .then(function (subscription) {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(subscription)
+    })
+    .catch(function (err) {
+      if (err.code === "SQLITE_CONSTRAINT_UNIQUE") {
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          info: {
+            id: 'subscriptions-already-exists',
+            message: 'The provided subscription was already stored.',
+          },
+        });
+      } else {
+        res.status(500);
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          error: {
+            id: 'unable-to-save-subscription',
+            message: 'The subscription was received but we were unable to save it to our database.',
+          },
+        });
+      }
+    });
+});
+
+// const isValidSaveRequest = (req: Request, res: Response) => {
+//   if (!req.body || !req.body.endpoint) {
+//     res.status(400);
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(
+//       JSON.stringify({
+//         error: {
+//           id: 'no-endpoint',
+//           message: 'Subscription must have an endpoint.',
+//         },
+//       }),
+//     );
+//     return false;
+//   }
+//   return true;
+// };
+
+export default router;
